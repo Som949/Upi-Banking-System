55aa0cd96cb4cad46edee61caa49d74cf6d54ae6
Ved-sketch <vedgupta1806@gmail.com>
2026-04-04 00:11:42 +0530

    Made the admin home page frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import AdminLogin from './Pages/Bank/AdminLogin'
 import ChangePassword from './Pages/Bank/ChangePassword'
+import AdminHomePage from './Pages/Bank/AdminHomePage'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<AdminLogin />} />
+        <Route path="/adminDashboard" element={<AdminHomePage />} />
         <Route path="/changepassword" element={<ChangePassword />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
